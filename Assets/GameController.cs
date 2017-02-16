b5a6a95fc2f2734442c701702bb93631e2b28fe1
mwalden <tont0r@hotmail.com>
2017-02-16 14:47:34 -0500

    fixed circle issue

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -5,14 +5,17 @@ public class GameController : MonoBehaviour {
 	public Rigidbody2D player;
 	private GameObject[] platforms;
 	void Start () {
-		player = GameObject.FindGameObjectWithTag ("Player").GetComponent<Rigidbody2D>();
+		GameObject g = GameObject.FindGameObjectWithTag ("Player");
+		player = g.GetComponent<Rigidbody2D> ();
+
 		platforms = GameObject.FindGameObjectsWithTag ("platform");
 		Debug.Log ("player null? " + player == null);
-			
+		Debug.Log ("platforms : " + platforms.Length);
 	}
 	
 	void Update () {
 		foreach (GameObject go in platforms){
+			Debug.Log (go.tag);
 			BoxCollider2D coll = go.GetComponent<BoxCollider2D> ();
 			coll.enabled = !(player.velocity.y > .3);			
 		}
